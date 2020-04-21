6fc75a98fa845a9b408858645232acd2d529d105
2uijin <49094082+2uijin@users.noreply.github.com>
2020-04-21 12:17:36 +0900

    초기 커밋.

---
+++ HelloConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloConsole 
+//namespace : 자바의 패키지
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World? 안녕하세요");
+        }
+    }
+}
